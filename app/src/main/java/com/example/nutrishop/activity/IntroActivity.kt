bad147825acb0eab58cf20d7c5471cf598db2525
Charindu Ranasingh <charindu2001@gmail.com>
2024-09-19 18:17:20 +0530

    Intro Act

--- app/src/main/java/com/example/nutrishop/activity/IntroActivity.kt
+++ app/src/main/java/com/example/nutrishop/activity/IntroActivity.kt
@@ -1,16 +1,28 @@
 package com.example.nutrishop.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nutrishop.MainActivity
 import com.example.nutrishop.R
+import com.example.nutrishop.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_intro)
+        binding.startButton.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java ))
+        }
 
 }
+
+}
+
