0c2635690cafc6390c0a3770f678d62b17159d7a
Charindu Ranasingh <charindu2001@gmail.com>
2024-10-02 16:04:28 +0530

    Item model parcelable

--- app/src/main/java/com/example/nutrishop/Model/ItemModel.kt
+++ app/src/main/java/com/example/nutrishop/Model/ItemModel.kt
@@ -1,14 +1,33 @@
 package com.example.nutrishop.Model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 
 data class ItemModel(
     var title: String = "",
     var description: String = "",
     var picUrl: String = "",
     var price: Double = 0.0,
-    var numberInCart: Int = 0,
     var rating: Double = 0.0,
+    var numberInCart: Int = 0,
     var nutrients: MutableList<Nutrient> = mutableListOf()
 
+):Parcelable{
+    constructor(parcel: Parcel):this(
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readDouble(),
+        parcel.readDouble()
     )
+
+    override fun describeContents(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        TODO("Not yet implemented")
+    }
+}
 
