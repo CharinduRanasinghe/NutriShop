7eab4fbf4a52c72a50792153109a62e1e51ab790
Charindu Ranasingh <charindu2001@gmail.com>
2024-10-04 22:08:09 +0530

    Category menu works

--- app/src/main/java/com/example/nutrishop/activity/MainActivity.kt
+++ app/src/main/java/com/example/nutrishop/activity/MainActivity.kt
@@ -6,8 +6,10 @@ import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -40,6 +42,11 @@ class MainActivity : BaseActivity() {
         searchButton.setOnClickListener {
             toggleSearch()
         }
+
+        findViewById<TextView>(R.id.menu_loader1).setOnClickListener {
+            startActivity(Intent(this, CategoriesActivity::class.java))
+        }
+
 
 
         initBanner()
